6231840a0fe3276256173e07495c6ec3f973e246
Joshua Stuckey <stuckeyjp1@gmail.com>
2025-09-18 11:42:16 -0400

    clarifications on in-progress work

--- thunder_averages.py
+++ thunder_averages.py
@@ -2,3 +2,4 @@
 #should fit in memory just fine
 #can loop through and perform calculations without opening new files
 #make the lambda function as simple as possible
+#consider the effect of sample size on using data with low amounts of data points
